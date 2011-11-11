5c0925fd1082c38ae8361376138f934760229882
Juho Vepsalainen <bebraw@gmail.com>
2011-11-11 13:31:09 +0200

    Add caching to sliceable

--- iterplus/sliceable.py
+++ iterplus/sliceable.py
@@ -6,6 +6,7 @@ def sliceable(iterator):
 
         def __init__(self, iter):
             self._iter, self._root = tee(iter)
+            self._cache = []
 
         def __getitem__(self, k):
             if isinstance(k, slice):
@@ -14,12 +15,19 @@ def sliceable(iterator):
             return self._get_value(k)
 
         def _get_value(self, k):
-            # XXX: might want to cache found values
+            cache_len = len(self._cache)
+
+            if k < cache_len:
+                return self._cache[k]
+
             self._root, root_copy = tee(self._root)
             ret = None
 
-            for i in xrange(k + 1):
+            for i in xrange(k - cache_len + 1):
                 ret = root_copy.next()
+                self._cache.append(ret)
+
+            self._root = root_copy
 
             return ret
 
